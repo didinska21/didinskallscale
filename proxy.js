ff8d87781cf81a9908b88ecb12f2af3ffcca02e5
didinska21 <mr.didinska21@gmail.com>
2025-12-29 00:54:13 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,19 +1,15 @@
 const fs = require("fs");
 
 function parseProxy(proxy) {
-  // support:
-  // ip:port
-  // user:pass@ip:port
   return proxy.includes("@")
     ? `http://${proxy}`
     : `http://${proxy}`;
 }
 
-function loadProxy(type = "static") {
+function loadProxy(type) {
   if (!fs.existsSync("proxy.txt")) return null;
 
-  const proxies = fs
-    .readFileSync("proxy.txt", "utf-8")
+  const proxies = fs.readFileSync("proxy.txt", "utf-8")
     .split("\n")
     .map(p => p.trim())
     .filter(Boolean);
@@ -21,8 +17,9 @@ function loadProxy(type = "static") {
   if (!proxies.length) return null;
 
   if (type === "rotating") {
-    const random = proxies[Math.floor(Math.random() * proxies.length)];
-    return parseProxy(random);
+    return parseProxy(
+      proxies[Math.floor(Math.random() * proxies.length)]
+    );
   }
 
   return parseProxy(proxies[0]); // static
